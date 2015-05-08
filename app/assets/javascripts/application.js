568a2fbdb9891809f21ebe452c9fc3bc13d869c7
Kyle Belanger <belanger.kyle@gmail.com>
2015-05-07 23:55:21 -0400

    persistant words in list

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,15 +20,19 @@ angular.module('d-angular', ['ui.router', 'templates'])
 			  listPromise: ['lists', function(lists){
 			    return lists.getAll();
 			  }]
-		  },
-
+		  }
 		})
 
 		// Lists state
 		.state('lists', {
-		  url: '/lists{id}',
-		  templateUrl: 'lists.html',
-		  controller: 'ListsCtrl'
+		  url: '/lists/{id}',
+		  templateUrl: 'list.html',
+		  controller: 'ListsCtrl',
+			resolve: {
+			  list: ['$stateParams', 'lists', function($stateParams, lists) {
+			    return lists.get($stateParams.id);
+			  }]
+			}
 		})
 
 		$urlRouterProvider.otherwise('home');
@@ -52,6 +56,7 @@ angular.module('d-angular', ['ui.router', 'templates'])
 			});
 		};
 
+		// get specific list
 		o.get = function(id) {
 		  return $http.get('/lists/' + id + '.json').then(function(res){
 		    return res.data;
@@ -63,6 +68,11 @@ angular.module('d-angular', ['ui.router', 'templates'])
 		  return $http.post('/lists.json', post).success(function(data){
 		    o.lists.push(data);
 		  });
+		};
+
+		// add word to list
+		o.addWord = function(id, word) {
+		  return $http.post('/lists/' + id + '/words.json', word);
 		};
 
 	  	return o;
@@ -81,8 +91,6 @@ angular.module('d-angular', ['ui.router', 'templates'])
 		// array of lists
 		$scope.lists = lists.lists;
 
-		// get list by ID
-		$scope.list = lists.lists[$stateParams.id];
 
 		// Add list function
 		// Creates a new list
@@ -101,69 +109,35 @@ angular.module('d-angular', ['ui.router', 'templates'])
 			$scope.title = '';
 		};
 
-		// Add word function
-		// Adds word to specific list based on list scope
-		$scope.addWord = function(){
 
-			// API URL
-			var api_url = "https://www.googleapis.com/scribe/v1/research?key=AIzaSyDqVYORLCUXxSv7zneerIgC2UYMnxvPeqQ&dataset=dictionary&dictionaryLanguage=en&query=";
-
-			// get data from API
-			$http.get(api_url + $scope.word)
-
-				// handle successful
-				.success(function (response) {
-					// push new word to array
-					$scope.list.words.push({
-						title: $scope.word,
-						date: new Date().toJSON().slice(0,10),
-						// meta
-						display: response.data[0]["groupResult"]["displayName"],
-						sound: "",
-						speech: "",
-						definitions: response.data[0]["dictionary"]["definitionData"][0]["meanings"]
-					});
-
-					// reset title
-					$scope.word = '';
-				});
-
-		};
 	}
 
 ])
 
 // Lists controller
 // ------------------------------
-.controller('ListsCtrl', ['$scope', '$stateParams', 'lists', '$http',
+.controller('ListsCtrl', ['$scope', 'lists', 'list', '$http',
 
 	// Main scope (used in views)
-	function($scope, $stateParams, lists, $http){
+	function($scope, lists, list, $http){
 		// get list by ID
-		$scope.list = lists.lists[$stateParams.id];
+		$scope.list = list;
 
-		// Add comment function
+		// Add word function
 		$scope.addWord = function(){
 
-			// API URL
-			var api_url = "https://www.googleapis.com/scribe/v1/research?key=AIzaSyDqVYORLCUXxSv7zneerIgC2UYMnxvPeqQ&dataset=dictionary&dictionaryLanguage=en&query=";
+			lists.addWord(list.id, {
+			  	title: $scope.title,
+				date: new Date().toJSON().slice(0,10),
+			})
+			.success(function(word) {
+				$scope.list.words.push(word);
+			});
 
-			// get data from API
-			$http.get(api_url + $scope.title)
-				// handle successful
-				.success(function (response) {
-					// push new list to array
-					$scope.list.words.push({
-						title: $scope.title,
-						date: new Date().toJSON().slice(0,10),
-						// meta
-						display: response.data[0]["groupResult"]["displayName"],
-						sound: "",
-						speech: "",
-						definitions: response.data[0]["dictionary"]["definitionData"][0]["meanings"]
-					});
-				});
+			$scope.title = '';
 		};
 	}
 
 ]);
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,43 @@
+require 'httparty'
+
 module ApplicationHelper
 
 	# page-title helper
 	def full_title(page_title)
-		
-	base_title = "Diction Project";
-	
+		base_title = "Diction Project";
 		if page_title.empty?
 			base_title		
 		else 
 			"#{base_title} - #{page_title}"
 		end 		
-
 	end
+
+	# Define method gets word data from Google's API
+	def define(word)  
+
+		# Requirements
+		include HTTParty
+
+		# Local scope varibale
+		data = []
+		  # Set variables
+		  base_url = "https://www.googleapis.com/scribe/v1/research?key=AIzaSyDqVYORLCUXxSv7zneerIgC2UYMnxvPeqQ&dataset=dictionary&dictionaryLanguage=en&query="
+		  # Get the payload
+		  response = HTTParty.get("#{base_url}" + "#{word}")
+		  
+		  # Catch response errors
+		  if response["data"] == nil
+		    error = { error: true, word: word }
+		    data.push(error)        
+		  else
+		    # Push response to data array
+		    data.push(response)
+		  end
+		return data
+	end
+
+
+
+
 	
 end
